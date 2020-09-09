0199cfa200afa53a903160190f1cd40b57e6d20a
Bushra Shaikh <iambushra37@gmail.com>
2020-09-09 23:55:28 +0530

    initial commit

---
+++ sketch.js
@@ -0,0 +1,67 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var particles = [];
+var plinkos = [];
+var divisions = [];
+var divisionHeight = 300;
+
+function setup() {
+  createCanvas(480, 800);
+  engine = Engine.create();
+  world = engine.world;
+
+  ground = new Ground(width / 2, height, 480, 20);
+
+  //creating divisions
+  for (var k = 0; k <= width; k = k + 80) {
+    divisions.push(new Divisions(k, height - divisionHeight / 2, 10, divisionHeight));
+  }
+
+  //creating plinkos
+  //row1
+  for (var i = 40; i < width; i = i + 50) {
+    plinkos.push(new Plinko(i, 75));
+  }
+  //row2
+  for (var i = 15; i < width; i = i + 50) {
+    plinkos.push(new Plinko(i, 175));
+  }
+  //row3
+  for (var i = 40; i < width; i = i + 50) {
+    plinkos.push(new Plinko(i, 275));
+  }
+  //row4
+  for (var i = 15; i < width; i = i + 50) {
+    plinkos.push(new Plinko(i, 375));
+  }
+
+
+}
+
+function draw() {
+  background(0, 0, 0);
+  Engine.update(engine);
+
+  //creating particles
+  if (frameCount % 60 === 0) {
+    particles.push(new Particle(random((width / 2) - 100, (width / 2) + 100), 10, 10))
+  }
+
+  ground.display();
+
+
+  for (var k = 0; k < divisions.length; k++) {
+    divisions[k].display();
+  }
+
+  for (var j = 0; j < particles.length; j++) {
+    particles[j].display();
+  }
+  for (var i = 0; i < plinkos.length; i++) {
+    plinkos[i].display();
+  }
+}
